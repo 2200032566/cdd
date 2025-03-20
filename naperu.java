797b3fdccede820334ae64788e9ca013092c1b0e
2200032566 <2200032566cser@gmail.com>
2025-03-20 12:20:36 +0530

    Update naperu.java

--- naperu.java
+++ naperu.java
@@ -1,5 +1,5 @@
 public class naperu{
       public static void main(String[]args){
-       System.out.println("Na inti peru penugonda na uru peru vinukonda na pettu peru anachodna!!");
+       System.out.println("Na inti peru penugonda na uru peru vinukonda na pettu peru anacodna!!");
 }
 }
